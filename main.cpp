d6f96ebc16d5be47536464873b53171b1195906a
Alexander <aciesla1231@gmail.com>
2023-10-20 14:32:42 +0200

    Added if, deleted some stuff too

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, again!" << std::endl;
-    
+
     for (int i=0; i<= 10; i++){
         std::cout<<"Hello number "<< i+1<<std::endl;
 
     }
-
+    if(1==2){
+        std::cout<<"Huh?"<<std::endl;
+    }
     return 0;
 }
