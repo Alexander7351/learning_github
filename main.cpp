ab46e91bd11358c6e7f29527e764bfbddd239472
Alexander <aciesla1231@gmail.com>
2023-10-20 14:13:33 +0200

    Adding a loooooop

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, again!" << std::endl;
+    
+    for (int i=0; i<= 10; i++){
+        std::cout<<"Hello number "<< i+1<<std::endl;
+
+    }
 
     return 0;
 }
